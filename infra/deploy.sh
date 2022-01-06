b19948d95e669d59d04a7b9065fd55335ee1a735
Mark Koelle <mkoelle@gmail.com>
2022-01-06 13:27:38 -0600

    add infra scripts

---
+++ infra/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set +x #xtrace
+set +v #verbose
+set -e #errexit
+set -u #nounset
+
+region='us-east-1'
+export AWS_DEFAULT_REGION=$region
+export AWS_REGION=$region
+export DOMAIN=mkoelle
+
+# export AWS_SECRET_ACCESS_KEY= 
+# export AWS_ACCESS_KEY_ID=  
+
+set -x
+
+aws cloudformation deploy \
+  --region ${region} \
+  --template-file cfn/infra.yml \
+  --stack-name com-${DOMAIN} \
+  --tags \
+      Owner=mkoelle \
+      Project=com.${DOMAIN} \
+      Code=com.mkoelle
