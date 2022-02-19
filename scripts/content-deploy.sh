8adf7f9bc91274c69965637f846b04033d8ef902
Mark Koelle <mkoelle@gmail.com>
2022-02-18 23:50:36 -0600

    deploy features into sub-bucket

--- scripts/content-deploy.sh
+++ scripts/content-deploy.sh
@@ -17,8 +17,12 @@ set -x
 npm run build
 
 BRANCH=$(git branch --show-current)
-FEATURE="$(echo $BRANCH | cut -d'/' -f2)"
-echo $FEATURE
+
+if [[ $BRANCH =~ "feature" ]]; then
+  FEATURE="/$(echo $BRANCH | cut -d'/' -f2)"
+  echo $FEATURE
+fi
+
 BUCKET=$(aws cloudformation list-exports --query "Exports[?Name == 'com-${DOMAIN}-content-bucket'].Value" --output text)
-aws s3 sync dist "s3://${BUCKET}" --exclude index.html --delete
-aws s3 cp dist "s3://${BUCKET}" --recursive --exclude "*" --include "index.html" --cache-control 'max-age=0' 
+aws s3 sync dist "s3://${BUCKET}${FEATURE}" --exclude index.html --delete
+aws s3 cp dist "s3://${BUCKET}${FEATURE}" --recursive --exclude "*" --include "index.html" --cache-control 'max-age=0' 
