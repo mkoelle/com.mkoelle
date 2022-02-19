f96c1b9eddb7abe68fc9a8f7b8065d3a0c81032e
Mark Koelle <mkoelle@gmail.com>
2022-02-18 21:34:26 -0600

    run server as part of playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -97,13 +97,15 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: 'test-results/'
+  outputDir: 'test-results/',
+}
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+if (process.env.CI) {
+  /* Run the local dev server before starting the tests */
+  config.webServer = {
+    command: 'npm run preview',
+    port: 3000,
+  }
 }
 
 export default config
